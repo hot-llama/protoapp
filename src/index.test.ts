72f6505646f98bad11b50f7cc07d8d5f9c2d4ec1
jmedran <jmedran@gmail.com>
2016-10-29 19:21:38 -0700

    adding enzyme

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,11 @@
 import test from 'ava'
+import React from 'react'
+import { shallow } from 'enzyme';
+// import Hello from './components/hello';
+
+// test('mount', t => {
+//     const wrapper = mount(<Hello />)
+// });
 
 test('foo', t => {
     t.pass();
